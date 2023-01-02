271d4bd9f23d8696242a7e6979fd920792287d1f
Jaimish Dholariya <jdholariya374@rku.ac.in>
2023-01-02 22:16:37 +0530

    all

---
+++ app/src/main/java/com/example/fitnessworkout/Femaleshowdata.java
@@ -0,0 +1,47 @@
+package com.example.fitnessworkout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class Femaleshowdata extends AppCompatActivity {
+
+
+    TextView name,desc;
+    LottieAnimationView animation;
+    ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_femaleshowdata);
+
+
+        name = findViewById(R.id.name);
+        desc = findViewById(R.id.desc);
+        animation = findViewById(R.id.animation);
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back  = new Intent(Femaleshowdata.this,Femaleworkout.class);
+                startActivity(back);
+                finish();
+            }
+        });
+
+        Intent card = getIntent();
+        name.setText(card.getStringExtra("Name"));
+        desc.setText(card.getIntExtra("Description",0));
+        animation.setAnimation(card.getIntExtra("Animation",0));
+
+
+    }
+}
